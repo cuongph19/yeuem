7adc10b8fa2d6cd78043463ecaa9ba8a187578b9
cuongph19 <trancuong.alok@gmail.com>
2024-11-05 11:53:20 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Em có tin vào tình yêu từ cái nhìn đầu tiên không? Hay anh cần phải đi qua trước mặt em một lần nữa?`,
+    btnIntro: 'hihi',
+    title: 'em ưi    phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh zai ạ:3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu anh nhiều lắm lắm luôn <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối mai anh qua đón nhé công chúa.',
+    btnAccept: 'Dạaaaaa <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100035327810726' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
